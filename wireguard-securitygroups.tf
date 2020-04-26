2198c3f72040df5e85c86f9d116ac6096d63988a
Jim Hall <jim.hall.dev@gmail.com>
2020-04-26 19:31:36 -0400

    add internet access

--- wireguard-securitygroups.tf
+++ wireguard-securitygroups.tf
@@ -13,6 +13,18 @@ resource "aws_security_group" "sg_wireguard_external" {
   ingress {
     from_port   = var.wg_server_port
     to_port     = var.wg_server_port
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = var.wg_server_port
+    to_port     = var.wg_server_port
+    protocol    = "icmp"
+    cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port   = var.wg_server_port
+    to_port     = var.wg_server_port
     protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
